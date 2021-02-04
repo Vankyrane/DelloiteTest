961035ce36ea704f1001c4461545e22d4436e6fb
Vankyrane <prasaddrane@gmail.com>
2021-02-04 19:15:09 +0000

    project initiated

---
+++ deloitte/src/test/java/com/deloitte/deloitte/DeloitteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.deloitte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloitteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
